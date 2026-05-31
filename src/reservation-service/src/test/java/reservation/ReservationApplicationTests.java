7a94a8f0476b585bed48c22f873a6d4577eaa020
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-31 20:43:03 +0200

    ci(fix): turn off auto-config of db

--- src/reservation-service/src/test/java/reservation/ReservationApplicationTests.java
+++ src/reservation-service/src/test/java/reservation/ReservationApplicationTests.java
@@ -2,8 +2,12 @@ package reservation;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class ReservationApplicationTests {
 
     @Test
