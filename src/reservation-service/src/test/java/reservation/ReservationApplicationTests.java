4268beda72d21c1b1b67688053263c74e418834d
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-06-25 21:45:17 +0200

    fix(test): mock SeatRepository in context load test to avoid DB dependency

--- src/reservation-service/src/test/java/reservation/ReservationApplicationTests.java
+++ src/reservation-service/src/test/java/reservation/ReservationApplicationTests.java
@@ -2,16 +2,15 @@ package reservation;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reservation.repository.SeatRepository;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.autoconfigure.exclude=" +
-    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-    "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration"
-})
 class ReservationApplicationTests {
+
+    // mock SeatRepository so Spring doesn't need a real database to start
+    @MockBean
+    private SeatRepository seatRepository;
 
     @Test
     void contextLoads() {
